e77dc8f6cb1e61dcc4bb856c6723efc347e89822
yaser samsam <yaser.alsamsam@gmail.com>
2024-11-20 01:39:00 +0400

    first commit

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -21,7 +21,11 @@ use Illuminate\Support\Facades\File;
 use App\Models\Suggest;
 use App\Http\Resources\SuggestResource;
 use App\Http\Resources\ProblemResource;
+use App\Models\Detail;
+use App\Models\Number;
 use App\Models\Problem;
+use App\Models\Skil;
+use App\Models\Social;
 use App\Models\Traffic;
 
 class UserController extends Controller
@@ -61,8 +65,6 @@ class UserController extends Controller
                 ]);
                 $org=new Organization();
                 $org->experience=$req->experience;
-                $org->details=$req->details;
-                $org->skils=$req->skils;
                 // upload one image
                 if($req->hasfile('logo')) {  
                     $file=$req->file('logo');
@@ -89,13 +91,25 @@ class UserController extends Controller
                 //
                 $org->view=$req->view;
                 $org->message=$req->message;
-                $org->number=$req->number;
-                $org->socials=$req->socials;
                 $org->address=$req->address;
                 $org->phone=$req->phone;
-                $org->complaints=$req->complaints;
-                $org->suggests=$req->suggests;
+                $data=[];
                 $newOrg->organization()->save($org);
+                foreach($req->details as $det) 
+                   array_push($data,new Detail(['text'=>$det->text]));
+                $newOrg->organization->details()->saveMany($data);
+                $data=[];
+                foreach($req->skils as $skil) 
+                   array_push($data,new Skil(['text'=>$skil->text]));        
+                $newOrg->organization->skils()->saveMany($data);
+                $data=[];
+                foreach($req->number as $number) 
+                   array_push($data,new Number(['type'=>$number->type,'number'=>$number->number]));
+                $newOrg->organization->numbers()->saveMany($data);    
+                $data=[];
+                foreach($req->socials as $socials) 
+                   array_push($data,new Social(['type'=>$socials->type,'url'=>$socials->url]));
+                $newOrg->organization->socials()->saveMany($data);    
                 if(sizeof($imgs)!==0)
                    $newOrg->organization->images()->saveMany($imgs);
                 return response()->json(['message'=>'added success'],201);
